79086eba3e2161fc2c7bcc97b08cb89d45662084
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-23 16:46:35 +0200

    upd

---
+++ Calendar/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    public partial class MainWindow : Window
+    {
+        private int currentMonth = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayCalendar(DateTime.Now);
+            previousMonthButton.Click += (s, e) => DisplayCalendar(DateTime.Now.AddMonths(--currentMonth));
+            nextMonthButton.Click += (s, e) => DisplayCalendar(DateTime.Now.AddMonths(++currentMonth));
+        }
+        private void DisplayCalendar(DateTime monthToShow)
+        {
+            calendarGrid.Children.Clear();
+
+            for (int i = 1; i <= DateTime.DaysInMonth(monthToShow.Year, monthToShow.Month); i++)
+            {
+                var dayButton = new Button
+                {
+                    Content = i.ToString(),
+                    Tag = new DateTime(monthToShow.Year, monthToShow.Month, i)
+                };
+                dayButton.FontSize = 30;
+                dayButton.FontWeight = FontWeights.Bold;
+
+                dayButton.Click += (s, e) =>
+                {
+                    var selectedDate = (DateTime)dayButton.Tag;
+                    MessageBox.Show($"date selected: {selectedDate.ToShortDateString()}");
+                };
+
+                calendarGrid.Children.Add(dayButton);
+            }
+            monthTextBlock.Text = monthToShow.ToString("MMMM yyyy");
+        }
+    }
+}
---
+++ TipsCalculator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TipsCalculator
+{
+    public partial class MainWindow : Window
+    {
+        private decimal tipPercentage = 1;
+        private readonly Regex numericRegex = new Regex(@"^\d+(\.\d+)?$");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculateButton.IsEnabled = false;
+
+            calculateButton.Click += (s, e) =>
+            {
+                if (numericRegex.IsMatch(inputTextBox.Text))
+                {
+                    decimal dinnerPrice = decimal.Parse(inputTextBox.Text);
+                    outputTextBlock.Text = (dinnerPrice * tipPercentage).ToString("C");
+                    calculateButton.IsEnabled = false;
+                    inputTextBox.Clear();
+                }
+                else { MessageBox.Show("Invalid input. Enter valid dinner's prise."); }
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button tipButton)
+            {
+                tipPercentage *= 1 / decimal.Parse(tipButton.Tag.ToString());
+                calculateButton.IsEnabled = true;
+            }
+        }
+    }
+}
